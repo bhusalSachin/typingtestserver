e179f75270fa7a14b20f527c91e41e88c482bf77
2bitCode <sachinb1377@gmail.com>
2022-01-24 18:19:30 +0545

    port 5000

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const server = require("http").createServer();
+const server = require("http").createServer((req, res) => {
+  res.end("Hello world");
+});
 
 const io = require("socket.io")(server, {
   transports: ["websocket", "polling"],
@@ -8,4 +10,8 @@ const users = {};
 
 io.on("connection", (client) => {
   console.log("Connected");
+});
+
+server.listen(5000, () => {
+  console.log("running on 5000");
 });
